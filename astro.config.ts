fb166932a9d50fa06d2d58664e6c726e18fe6033
Fractal-Tess <vgfractal@gmail.com>
2024-09-22 23:32:50 +0300

    remove maskable image

--- astro.config.ts
+++ astro.config.ts
@@ -69,13 +69,13 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
           }
+          //{
+          //  src: 'pwa-512x512.png',
+          //  sizes: '512x512',
+          //  type: 'image/png',
+          //  purpose: 'any maskable'
+          //}
         ]
       },
       workbox: {

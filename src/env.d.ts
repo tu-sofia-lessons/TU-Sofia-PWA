8b0089f3ba14243f54cab442b9c02ec748eb9647
Fractal-Tess <vgfractal@gmail.com>
2024-10-04 15:19:59 +0300

    add env typing support interface

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,9 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
+
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
